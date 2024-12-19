ffb13fc1d30d82e86a5ed95770718fc808d89f47
abhishek <abhishp23@gmail.com>
2024-12-19 14:01:54 +0530

    refresh fix

--- src/contexts/authContext.jsx
+++ src/contexts/authContext.jsx
@@ -1,31 +1,31 @@
-import React, { createContext, useReducer } from 'react';
-import auth, { initialState } from '../store/accountReducer'; 
+import React, { createContext, useReducer, useEffect } from 'react';
+import auth, { initialState } from '../store/accountReducer';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(auth, {
-      ...initialState,
-      isLoggedIn: !!localStorage.getItem('token'),
-      user: JSON.parse(localStorage.getItem('user')) || null,
-      
-    });
-  console.log(state);
-    // Save auth state to localStorage whenever it changes
-    React.useEffect(() => {
-      if (state.isLoggedIn) {
-        localStorage.setItem('user', JSON.stringify(state.user));
-        localStorage.setItem('token', state.token || '');
-      } else {
-        localStorage.removeItem('user');
-        localStorage.removeItem('token');
+  const [state, dispatch] = useReducer(auth, {
+    ...initialState,
+    isLoggedIn: !!localStorage.getItem('token'),
+    token: localStorage.getItem('token') || null,
+    user: JSON.parse(localStorage.getItem('user')) || null,
+  });
+
+  useEffect(() => {
+    if (state.isLoggedIn) {
+      if (state.token) {
+        localStorage.setItem('token', state.token);
       }
-    }, [state]);
-  
-    return (
-      <AuthContext.Provider value={{ state, dispatch }}>
-        {children}
-      </AuthContext.Provider>
-    );
-  };
-  
+      localStorage.setItem('user', JSON.stringify(state.user));
+    } else {
+      localStorage.removeItem('user');
+      localStorage.removeItem('token');
+    }
+  }, [state]);
+
+  return (
+    <AuthContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
